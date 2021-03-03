2965a50990ddf6932bbc4ffb5fe2bbcf44747222
Saharsh-R <48884865+Saharsh-R@users.noreply.github.com>
2021-03-03 17:02:28 +0530

    update link

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" target="_blank" href="https://saharsh-r.github.io/">
         Saharsh Rathi
       </Link>{' '}
       {new Date().getFullYear()}
